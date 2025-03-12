4239c7284d9686e18f56811481bcdede93ed4e30
Pilks-pixel <peterpilkington@hotmail.com>
2025-03-12 07:31:34 +0000

    added readstream

--- server/src/model/planets.ts
+++ server/src/model/planets.ts
@@ -1,21 +1,42 @@
-var planets: Array<{
-  kepler_name: string;
-  diameter: number;
-  distanceFromSun: number;
-  hasLife: boolean;
-}> = [
-  {
-    kepler_name: "Earth",
-    diameter: 12742,
-    distanceFromSun: 149.6e6, // in kilometers
-    hasLife: true,
-  },
-  {
-    kepler_name: "Mars",
-    diameter: 6779,
-    distanceFromSun: 227.9e6, // in kilometers
-    hasLife: false,
-  },
-];
+import { createReadStream } from "node:fs";
+import { parse } from "csv-parse";
+
+var planets: Planet[] = [];
+var parser = parse({
+  comment: "#",
+  columns: true,
+});
+
+type Planet = {
+  kepoi_name: string;
+  koi_disposition: string;
+  koi_insol: number;
+  koi_prad: number;
+};
+
+createReadStream("./kepler-data.csv").pipe(parser);
+
+parser.on("data", (data: Planet) => {
+  if (isHabitablePlanet(data)) {
+    planets.push(data);
+  }
+});
+
+parser.on("error", err => {
+  console.error("Error parsing CSV:", err);
+});
+
+parser.on("end", () => {
+  console.log("Parsing completed:", planets);
+});
+
+function isHabitablePlanet(planet: Planet): boolean {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 export default planets;

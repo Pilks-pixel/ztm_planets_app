7b336dcb1732e970f93c4f6fd236799e1bab52f6
Pilks-pixel <peterpilkington@hotmail.com>
2025-04-28 07:27:26 +0100

    added mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,27 @@
 import { createServer } from "node:http";
+import mongo from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 import app from "./app.ts";
 import { loadPlanetsData } from "./model/planets.ts";
+import mongoose from "mongoose";
 
 var port = process.env.PORT || 8000;
+var mongoUrl: string = process.env.MONGO_URL || "";
 var server = createServer(app);
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection opened");
+});
+mongoose.connection.on("error", err => {
+  console.error("MongoDB connection error:", err);
+});
 
 startServer();
 
 async function startServer() {
+  await mongo.connect(mongoUrl);
   await loadPlanetsData();
 
   server.listen(port, function () {
